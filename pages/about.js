702e998cfdbfe5fc066634ca13c8474132bc3354
Abdullah Karaman <abdullahkaramaniletisim@gmail.com>
2023-04-29 15:36:59 +0300

    directories

--- pages/about.js
+++ pages/about.js
@@ -5,23 +5,21 @@ import PageBanner from "../components/Common/PageBanner";
 import FeaturesThree from "../components/Features/FeaturesThree";
 import TeamTwo from "../components/Team/TeamTwo";
 import Testimonial from "../components/Testimonial/Testimonial";
-import ContentTwo from "./../components/AboutPage/ContentTwo";
+import ContentTwo from "../components/AboutPage/ContentTwo";
 
 export default function about() {
-	return (
-		<>
-			<Head>
-				<title>About Page</title>
-			</Head>
-			<PageBanner
-				title="About Us"
-				content="We work to create some of the most attractive and meaningful places for small businesses."
-			/>
-			<ContentOne />
-			<FeaturesThree />
-			<ContentTwo />
-			
-			
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <title>About Page</title>
+            </Head>
+            <PageBanner
+                title="About Us"
+                content="We work to create some of the most attractive and meaningful places for small businesses."
+            />
+            <ContentOne />
+            <FeaturesThree />
+            <ContentTwo />
+        </>
+    );
 }

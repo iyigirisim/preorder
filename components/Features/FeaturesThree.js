9e8523f53f2893627b479639562047c9eb9aa949
Ugur Akdogan <balicienugur@gmail.com>
2023-05-02 14:37:34 +0300

    modified:   components/Features/FeaturesThree.js

--- components/Features/FeaturesThree.js
+++ components/Features/FeaturesThree.js
@@ -2,14 +2,15 @@ export default function FeaturesThree() {
 	return (
 		<div className="container section-padding mtm-120">
 			<div className="row  d-flex justify-content-between">
+				<h2 className="text-center pb-5">Our Missions</h2>
 				<div className="col-xxl-3 col-xl-4 col-md-6">
 					<div className="single-about-features">
 						<div className="icon">
 							<img src="img/icons/idea.png" alt="" />
 						</div>
 						<div className="content">
-							<h3>Creative Thinking</h3>
-							<p>Creative thinking is the ability to consider something in a new way.</p>
+							<h3>First</h3>
+							<p>To produce and develop high-quality, environmentally friendly, and energy-efficient modular homes</p>
 						</div>
 					</div>
 				</div>
@@ -19,8 +20,8 @@ export default function FeaturesThree() {
 							<img src="img/icons/man.png" alt="" />
 						</div>
 						<div className="content">
-							<h3>Skilled Team</h3>
-							<p>We have a skilled team. Those who work through their own experience.</p>
+							<h3>Second</h3>
+							<p>To provide reliable and professional service by maintaining the highest level of customer satisfaction</p>
 						</div>
 					</div>
 				</div>
@@ -30,8 +31,8 @@ export default function FeaturesThree() {
 							<img src="img/icons/setting.png" alt="" />
 						</div>
 						<div className="content">
-							<h3>Maximum Service</h3>
-							<p>Maximum service has been in the creative industry for over 30 years.</p>
+							<h3>Third</h3>
+							<p>To progress towards leadership in the industry through innovation and continuous improvement</p>
 						</div>
 					</div>
 				</div>
